db55c50afde0618d234c47180f4e59a78c3523da
Lea Baumgart <lea@abmgrt.dev>
2020-12-20 23:28:59 +0100

    changed auth to micro api

--- utils/auth.js
+++ utils/auth.js
@@ -3,8 +3,10 @@ const axios = require("axios").default;
 module.exports = async (req) => {
     if (!req.headers.authorization) return;
     try {
-        const authData = await axios.post("https://sessions.alles.cc/", {
-            token: req.headers.authorization
+        const authData = await axios.get("https://micro.alles.cx/api/me", {
+            headers: {
+				"Authorization": req.headers.authorization
+			}
         });
         return authData.data;
     }
