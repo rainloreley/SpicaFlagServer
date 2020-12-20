f17f8ec27f96515ecb4941e73bad13cf70e8db73
Lea Baumgart <lea@abmgrt.dev>
2020-12-20 23:12:38 +0100

    changed authorization process to micro api

--- api/update.js
+++ api/update.js
@@ -4,33 +4,35 @@ const allowedRings = require("../data/allowedRings.json");
 const spicaCredits = require("../data/spicaCredits.json");
 
 module.exports = async (req, res) => {
-    const authUser = await auth(req);
-    if (!authUser || authUser.user !== req.params.id) return res.status(401).json({ err: "badAuthorization" });
-    if (typeof req.params.id !== "string" ||
-    req.params.id.trim().length === 0 ||
-    typeof req.body.ring !== "string" ||
-    !allowedRings.includes(req.body.ring)
-    ) return res.status(400).json({ err: "badRequest" });
+	const authUser = await auth(req);
+	if (!authUser || authUser.id !== req.params.id)
+		return res.status(401).json({ err: "badAuthorization" });
+	if (
+		typeof req.params.id !== "string" ||
+		req.params.id.trim().length === 0 ||
+		typeof req.body.ring !== "string" ||
+		!allowedRings.includes(req.body.ring)
+	)
+		return res.status(400).json({ err: "badRequest" });
 
-    if (req.body.ring === "supporter" && !spicaCredits.includes(req.params.id)) return res.status(401).json({err: "notAllowed"});
+	if (req.body.ring === "supporter" && !spicaCredits.includes(req.params.id))
+		return res.status(401).json({ err: "notAllowed" });
 
-    const user = await db.User.findOne({
-        where: {
-            uid: req.params.id
-        }
-    });
-    if (!user) {
-        const newUser = await db.User.create({
-            uid: req.params.id,
-            ring: req.body.ring
-        });
-        return res.send(newUser);
-    }
-    else {
-        await user.update({
-            ring: req.body.ring
-        })
-        return res.send(user);
-    }
-
-}
+	const user = await db.User.findOne({
+		where: {
+			uid: req.params.id,
+		},
+	});
+	if (!user) {
+		const newUser = await db.User.create({
+			uid: req.params.id,
+			ring: req.body.ring,
+		});
+		return res.send(newUser);
+	} else {
+		await user.update({
+			ring: req.body.ring,
+		});
+		return res.send(user);
+	}
+};
--- utils/auth.js
+++ utils/auth.js
@@ -1,14 +1,13 @@
 const axios = require("axios").default;
 
 module.exports = async (req) => {
-    if (!req.headers.authorization) return;
-    try {
-        const authData = await axios.post("https://sessions.alles.cc/", {
-            token: req.headers.authorization
-        });
-        return authData.data;
-    }
-    catch(err) {
-        return;
-    }
-}
+	if (!req.headers.authorization) return;
+	try {
+		const authData = await axios.post("https://micro.alles.cx/api/me", {
+			token: req.headers.authorization,
+		});
+		return authData.data;
+	} catch (err) {
+		return;
+	}
+};
